01b89d61cdfb80943fdd98cb7d4dc9f91a1368fd
sharon zhao <xinrui.zhao@yahoo.com>
2020-09-19 11:44:31 -0400

    fixed

--- video/config/settings.py
+++ video/config/settings.py
@@ -136,7 +136,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
 # qiniu
-QINIU_AK =
-QINIU_SK =
-QINIU_VIDEO =
-QINIU_VIDEO_URL = 
+# QINIU_AK =
+# QINIU_SK =
+# QINIU_VIDEO =
+# QINIU_VIDEO_URL =
